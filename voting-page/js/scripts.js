7cddf3baeb7fcb25d4ff2cc236200928bd54fc68
Fausto Guillen and James Cho <james@>
2018-10-18 10:27:57 -0700

    animal wiki setup

--- voting-page/js/scripts.js
+++ voting-page/js/scripts.js
@@ -1,14 +1,16 @@
 $(function() {
-    $("form#add").submit(function(event) {
-    let age = parseInt($("input#add1").val());
-    if (age >= 18) {
+    $("form#form").submit(function() {
+    var choice = $("input#animal").val();
+      alert(choice);
+
+    /* if (age >= 18) {
       $("#adult").show();
       $("#underage").hide();
     } else {
       $("#underage").show();
       $("#adult").hide();
-    }   
-    event.preventDefault();
+    }    */
+    //event.preventDefault();
     });
 });
 
